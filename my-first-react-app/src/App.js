51487d3806cb3a0124a507c9868d552dfbf711da
FreddyMartinez <fhmartinezq@unal.edu.co>
2020-04-25 00:11:34 -0500

    sending action from child to parent

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import Search from './components/users/Search'
 import axios from 'axios'
 
 class App extends React.Component {
@@ -10,12 +11,20 @@ class App extends React.Component {
     loading: false
   }
 
-  async componentDidMount() {
+  // async componentDidMount() {
+  //   this.setState({loading: true});
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+  //   this.setState({users: res.data, loading: false});
+  // }
+
+  searchUsers = async (user) => {
     this.setState({loading: true});
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${user}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({users: res.data, loading: false});
+    this.setState({users: res.data.items, loading: false});
   }
 
   render() {
@@ -23,6 +32,7 @@ class App extends React.Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search searchUsers={this.searchUsers}/>
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
