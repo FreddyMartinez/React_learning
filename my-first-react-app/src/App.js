8d1c7c1adba6277fcd399ec0653353d9092db3ac
FreddyMartinez <fhmartinezq@unal.edu.co>
2020-04-27 19:46:10 -0500

    Adding clear and alert functionalities

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
-import Search from './components/users/Search'
-import axios from 'axios'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     users: [],
-    loading: false
-  }
+    loading: false,
+    alert: null,
+  };
 
   // async componentDidMount() {
   //   this.setState({loading: true});
@@ -19,21 +22,35 @@ class App extends React.Component {
   //   this.setState({users: res.data, loading: false});
   // }
 
-  searchUsers = async (user) => {
-    this.setState({loading: true});
+  searchUsers = async user => {
+    this.setState({ loading: true });
     const res = await axios.get(
       `https://api.github.com/search/users?q=${user}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({users: res.data.items, loading: false});
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  clearUsers = () => this.setState({ users: [] });
+
+  setAlert = (message, type) => {
+    this.setState({ alert: { msg: message, type } });
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
 
   render() {
+    const { users, loading, alert } = this.state;
     return (
-      <div className="App">
+      <div className='App'>
         <Navbar />
-        <div className="container">
-          <Search searchUsers={this.searchUsers}/>
-          <Users loading={this.state.loading} users={this.state.users} />
+        <div className='container'>
+          <Alert alert={alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- my-first-react-app/src/components/users/Search.js
+++ my-first-react-app/src/components/users/Search.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class Search extends Component {
   state = {
-    text: ''
-  }
+    text: "",
+  };
 
-  onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value});
-  }
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
   onSubmit = e => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-  }
+    if (this.state.text == "") {
+      this.props.setAlert("You should write a name", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
+  };
 
   render() {
+    const { showClear, clearUsers } = this.props;
+
     return (
       <div>
         <form onSubmit={this.onSubmit} className='form'>
@@ -27,11 +34,16 @@ export class Search extends Component {
             onChange={this.onChange}
           />
           <input
-            type="submit"
+            type='submit'
             value='Search'
             className='btn btn-dark btn-block'
           />
         </form>
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
@@ -39,6 +51,9 @@ export class Search extends Component {
 
 Search.propTypes = {
   searchUsers: PropTypes.func.isRequired,
-}
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func,
+};
 
-export default Search
+export default Search;
