ae57aeaa870361f9adee26b1d6beb1072495afd5
Freddy Martinez <fmartinez@belatrixsf.com>
2020-05-22 20:07:05 -0500

    Creating db connection

--- ContactKeeper-back/server.js
+++ ContactKeeper-back/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
-
 const app = express();
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT | "5000";
 
+//Connect Mongo DB
+connectDB();
+
+//Default
 app.get("", (req, res) => {
   res.json({ msg: "Welcome to the Contact Keeper" });
 });
