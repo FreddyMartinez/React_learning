c4ca9e3a61a1449e601d7f31f246ec2e38be7c2e
Freddy Martinez <fmartinez@belatrixsf.com>
2020-05-25 15:33:28 -0500

    creating user model & including express-validator

--- ContactKeeper-back/routes/users.js
+++ ContactKeeper-back/routes/users.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
 
 /**
  * @Route         POST api/users
  * @description   creates new user
  * @access        Public
  */
-router.post("/", (req, res) => {
-  res.send("User register");
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check(
+      "password",
+      "Password with more than 6 characters is required"
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("user created");
+  }
+);
 
 module.exports = router;
--- ContactKeeper-back/server.js
+++ ContactKeeper-back/server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT | "5000";
 //Connect Mongo DB
 connectDB();
 
+//Init middleware
+app.use(express.json({ extended: false }));
+
 //Default
 app.get("", (req, res) => {
   res.json({ msg: "Welcome to the Contact Keeper" });
