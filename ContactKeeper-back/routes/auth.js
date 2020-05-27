9e43eba3a9e83b56e184e06c9bf76623af9fbdba
Freddy Martinez <fmartinez@belatrixsf.com>
2020-05-27 11:04:18 -0500

    create log in service that returns a token

--- ContactKeeper-back/routes/auth.js
+++ ContactKeeper-back/routes/auth.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 /**
  * @Route         GET api/auth
@@ -15,8 +20,58 @@ router.get("/", (req, res) => {
  * @description   auth user and get token
  * @access        Public
  */
-router.post("/", (req, res) => {
-  res.send("Log in user");
-});
+router.post(
+  "/",
+  [
+    check("email", "Please enter a valid email").isEmail(),
+    check(
+      "password",
+      "Password with more than 6 characters is required"
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: "This email does not exist" });
+      }
+
+      const correctPassword = await bcrypt.compare(password, user.password);
+
+      if (!correctPassword) {
+        return res.status(400).json({ msg: "Invalid credentials" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
