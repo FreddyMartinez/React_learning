47229abda18644d5ccee3245b5dee98eb9a4d43f
Freddy Martinez <fmartinez@belatrixsf.com>
2020-05-27 10:57:05 -0500

    using JWT to send a token when new user is created

--- ContactKeeper-back/routes/users.js
+++ ContactKeeper-back/routes/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const User = require("../models/User");
 
@@ -51,8 +53,27 @@ router.post(
 
       user.save();
 
-      res.send("User created");
-    } catch (error) {}
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
