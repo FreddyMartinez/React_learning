abcaf59359a89a8d415973a623d53973e9e09977
Freddy Martinez <fmartinez@belatrixsf.com>
2020-05-31 00:07:32 -0500

    Creating contacts and contact card components

--- ContactKeeper-back/client/src/components/pages/Home.js
+++ ContactKeeper-back/client/src/components/pages/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import Contacts from "../contacts/Contacts";
 const Home = () => {
-  return <div>Welcome</div>;
+  return (
+    <div className="grid-2">
+      <div>{/* Contact Form */}</div>
+      <div>
+        <Contacts></Contacts>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
