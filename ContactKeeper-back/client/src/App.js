f0b40a10f182532e899cae8827e14662fa558a85
Freddy Martinez <fmartinez@belatrixsf.com>
2020-07-07 19:01:17 -0500

    Creating Private routing component & updating nav

--- ContactKeeper-back/client/src/App.js
+++ ContactKeeper-back/client/src/App.js
@@ -6,6 +6,7 @@ import AuthState from "./context/auth/AuthState";
 import ContactState from "./context/contact/ContactState";
 import AlertState from "./context/alert/AlertState";
 import Navbar from "./components/layout/Navbar";
+import PrivateRoute from "./components/routing/PrivateRoute";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Register from "./components/auth/Register";
@@ -27,7 +28,7 @@ function App() {
               <div className="container">
                 <Alert />
                 <Switch>
-                  <Route exact path="/" component={Home} />
+                  <PrivateRoute exact path="/" component={Home} />
                   <Route exact path="/about" component={About} />
                   <Route exact path="/register" component={Register} />
                   <Route exact path="/login" component={Login} />
--- ContactKeeper-back/client/src/context/auth/AuthState.js
+++ ContactKeeper-back/client/src/context/auth/AuthState.js
@@ -90,7 +90,7 @@ const AuthState = props => {
   };
 
   // Logout
-  const logout = () => console.log("load");
+  const logout = () => dispatch({ type: LOGOUT });
 
   // Clear Errors
   const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
