d4a2daebfafada2bfc2a6031c611353a095ae0e1
Freddy Martinez <fmartinez@belatrixsf.com>
2020-07-14 23:42:59 -0500

    creating Add Log functionality

--- it-logger/src/components/logs/AddLogModal.js
+++ it-logger/src/components/logs/AddLogModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import M from "materialize-css/dist/js/materialize.min";
+import { connect } from "react-redux";
+import { addLog } from "../../actions/logActions";
+import PropTypes from "prop-types";
 
-const AddLogModal = () => {
+const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState("");
@@ -10,7 +13,16 @@ const AddLogModal = () => {
     if (message === "" || tech === "") {
       M.toast({ html: "Please enter a message and a tech" });
     } else {
-      console.log(`Submit ${message}, ${attention}, ${tech}`);
+      const newLog = {
+        message,
+        attention,
+        tech,
+        date: new Date()
+      };
+
+      addLog(newLog);
+
+      M.toast({ html: `Ļog added ${tech}` });
 
       setMessage("");
       setTech("");
@@ -83,9 +95,13 @@ const AddLogModal = () => {
   );
 };
 
+AddLogModal.propTypes = {
+  addLog: PropTypes.func.isRequired
+};
+
 const modalStyle = {
   width: "75%",
   height: "75%"
 };
 
-export default AddLogModal;
+export default connect(null, { addLog })(AddLogModal);
