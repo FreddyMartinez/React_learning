a14c3c2b7ac3c55b43c35b13d966725620c7d992
Freddy Martinez <fmartinez@belatrixsf.com>
2020-07-15 20:36:23 -0500

    delete tech functionality

--- it-logger/src/actions/techActions.js
+++ it-logger/src/actions/techActions.js
@@ -52,6 +52,27 @@ export const addTechs = tech => async dispatch => {
   }
 };
 
+// Add technician
+export const deleteTech = id => async dispatch => {
+  try {
+    setLoading();
+
+    await fetch(`/techs/${id}`, {
+      method: "DELETE"
+    });
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText
+    });
+  }
+};
+
 // Set Loading
 export const setLoading = () => {
   return {
